8174156a61a0f750b3f5150df5d893966a81c714
Krystian Stopka <kr6stopka@gmail.com>
2020-11-19 17:07:51 +0100

    simple slider

---
+++ 01_projects/app.js
@@ -0,0 +1,49 @@
+const slider = [
+  {
+    img: "./images/img1.jpg",
+  },
+  {
+    img: "./images/img2.jpg",
+  },
+  {
+    img: "./images/img3.jpg",
+  },
+];
+const img = document.querySelector(`.slider img`);
+const previousSlide = document.querySelector(`.slider .previous`);
+const nextSlide = document.querySelector(`.slider .next`);
+let activeSlide = 0;
+const time = 5000;
+
+const changeSlide = () => {
+  activeSlide++;
+  if (activeSlide === slider.length) {
+    activeSlide = 0;
+  }
+  console.log(activeSlide);
+  img.src = slider[activeSlide].img;
+};
+let indexInterval = setInterval(changeSlide, time);
+
+previousSlide.addEventListener(`click`, () => {
+  clearInterval(indexInterval);
+  activeSlide--;
+  if (activeSlide < 0) {
+    activeSlide = slider.length - 1;
+  }
+  img.src = slider[activeSlide].img;
+  console.log(activeSlide);
+  indexInterval = setInterval(changeSlide, time);
+});
+
+nextSlide.addEventListener(`click`, () => {
+  clearInterval(indexInterval);
+  activeSlide++;
+  console.log(activeSlide);
+  if (activeSlide === slider.length) {
+    activeSlide = 0;
+  }
+
+  img.src = slider[activeSlide].img;
+  indexInterval = setInterval(changeSlide, time);
+});
